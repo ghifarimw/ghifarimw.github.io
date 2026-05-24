f8b473034c5fc3df883ea1b83a9d2180931fda29
ghifarimw <ghifmw@gmail.com>
2026-05-24 22:18:06 +0700

    socials!

--- main.js
+++ main.js
@@ -5,28 +5,38 @@ function MobileCheck() {
         MobileRes = true;
         var ch = document.querySelectorAll(".card");
         ch.forEach((element) => {
+            if (element.classList.contains("exempt")) {
+                return;
+            }
             element.style.setProperty("flex-direction","column");
         });
+        try {
         document.getElementsByClassName("mp")[0].style.setProperty("flex-direction","column");
-        document.getElementsByClassName("mp")[0].style.setProperty("height","95vh");
+        document.getElementsByClassName("mp")[0].style.setProperty("height","75vh");
         document.getElementsByClassName("mp")[0].style.setProperty("text-align","center");
+        document.getElementById("face").style.setProperty("max-width","80%");
+        } catch {}
         document.getElementsByClassName("control")[0].style.setProperty("display","none");
         document.getElementsByClassName("love")[0].style.setProperty("display","none");
-        document.getElementById("face").style.setProperty("max-width","80%");
         document.getElementById("title-p").style.setProperty("text-align","center");
         document.getElementById("ttl").style.setProperty("width","100%");
     } else if (MobileRes && (window.innerHeight/window.innerWidth <= 1)) {
         MobileRes = false;
         var ch = document.querySelectorAll(".card");
         ch.forEach((element) => {
+            if (element.classList.contains("exempt")) {
+                return;
+            }
             element.style.setProperty("flex-direction","row");
         });
+        try {
         document.getElementsByClassName("mp")[0].style.setProperty("flex-direction","row");
         document.getElementsByClassName("mp")[0].style.setProperty("height","100vh");
         document.getElementsByClassName("mp")[0].style.setProperty("text-align","left");
+        document.getElementById("face").style.setProperty("max-width","55%");
+        } catch {}
         document.getElementsByClassName("control")[0].style.setProperty("display","flex");
         document.getElementsByClassName("love")[0].style.setProperty("display","inline");
-        document.getElementById("face").style.setProperty("max-width","55%");
         document.getElementById("title-p").style.setProperty("text-align","left");
         document.getElementById("ttl").style.setProperty("width","45%");
     }
